d917312b570ef584b8eb6c0ba66be02d87658860
msdesai187 <mxd4258@miami.edu>
2023-05-03 17:42:02 +0000

    About_Meghal

---
+++ R/about_meghal.R
@@ -0,0 +1,14 @@
+#' The `about_Meghal` Function
+#' @description This is four truths and one lie about Meghal.... The lie is about 03.
+#'
+#' @return NA
+#' @export about_Meghal
+about_Meghal <- function() {
+  cat("Which of these is a lie: \n")
+  cat("1. Meghal is vegeterian.\n")
+  cat("2. Meghal is from India.\n")
+  cat("3. Meghal is very good at cooking.\n")
+  cat("4. Meghal loves dancing.\n")
+  cat("5. Meghal loves travelling.\n")
+}
+
